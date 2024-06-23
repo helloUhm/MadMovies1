87161844ce3bd2399cb08d5116f2066c57721792
dup <142952077+helloUhm@users.noreply.github.com>
2024-06-23 18:53:38 -0500

    Update index.php

--- nextmovies.png/index.php
+++ nextmovies.png/index.php
@@ -3,10 +3,10 @@ require_once __DIR__ . '/app/index/indexRoutes.php';
 
 error_reporting(E_ALL & ~E_DEPRECATED);
 
-// NextMovies Public Routes
+// MadMovies Public Routes
 IndexRoutes::index();
 
 
-// NextMovies Admin Routes
+// MadMovies Admin Routes
 // TODO: Add Admin Routes
 
